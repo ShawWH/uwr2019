5d89f1b035b5f4468faf420544dc953e18341af7
renwoxingaishazao <51324590+renwoxingaishazao@users.noreply.github.com>
2019-06-10 20:50:13 +0800

    使用EasyMock建立一个DataSourceConfig类的一个Mock对象实例——王哲

--- 03-代码设计/test/core/generator/TestTemplateProcessor.java
+++ 03-代码设计/test/core/generator/TestTemplateProcessor.java
@@ -67,6 +67,9 @@ public class TestTemplateProcessor implements DataSourceType{
         //
         //
         // 这里写代码
+	DataSourceConfig mock = EasyMock.createMock(DataSourceConfig.class);
+        EasyMock.expect(mock.getConstDataSource()).andReturn(null);	
+	
 	dsc = new DataSourceConfig();
         PowerMockito.mockStatic(DataSourceConfig.class);
         PowerMockito.when(DataSourceConfig.newInstance()).thenReturn(dsc);
